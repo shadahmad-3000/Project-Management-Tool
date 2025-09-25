246cb01aa0cd3d75d311f3ef47761bba4a5ebeca
shadahmad-3000 <mohdshadahmad786@gmail.com>
2025-09-25 18:24:47 +0530

    remove unnecessary await

--- src/services/otp.service.js
+++ src/services/otp.service.js
@@ -4,138 +4,139 @@ const { OTP, User } = require("../models");
 const moment = require("moment");
 
 const sentOTP = async (body) => {
-    try {
-        const { email } = body;
-        const otp = Math.floor(100000 + Math.random() * 900000);
-        const expires = Date.now() + 5 * 60 * 1000;
+  try {
+    const { email } = body;
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    const expires = Date.now() + 5 * 60 * 1000;
 
-        const mailbody = {
-            from: '"AutoPe Payment Solutions',
-            to: email,
-            subject: "Your OTP Code",
-            text: `Your OTP is ${otp} and valid for 5 minutes.`
-        }
+    const mailbody = {
+      from: '"AutoPe Payment Solutions',
+      to: email,
+      subject: "Your OTP Code",
+      text: `Your OTP is ${otp} and valid for 5 minutes.`,
+    };
 
-        const mail = await mailer.sendMail(mailbody);
-        console.log("Mail sent", mail);
-        //otp to store in db
-        const otpData = {
-            email: email,
-            otp: otp,
-            expires: expires,
-            isUsed: false
-        }
-        await OTP.create(otpData);
+    const mail = await mailer.sendMail(mailbody);
+    console.log("Mail sent", mail);
+    //otp to store in db
+    const otpData = {
+      email: email,
+      otp: otp,
+      expires: expires,
+      isUsed: false,
+    };
+    await OTP.create(otpData);
 
-        return {
-            status: httpStatus.OK,
-            message: `OTP sent to ${email}`
-        }
-    } catch (error) {
-        console.error(error?.message || error);
-        return {
-            status: httpStatus.INTERNAL_SERVER_ERROR,
-            message: "Failed to sent OTP"
-        }
-    }
+    return {
+      status: httpStatus.OK,
+      message: `OTP sent to ${email}`,
+    };
+  } catch (error) {
+    console.error(error?.message || error);
+    return {
+      status: httpStatus.INTERNAL_SERVER_ERROR,
+      message: "Failed to sent OTP",
+    };
+  }
 };
 
 const resendOTP = async (body) => {
-    try {
-        const { email } = body;
-        const existingOTP = await OTP.findOne({ email }).sort({ _id: -1 });
-        if (existingOTP) {
-            const now = moment();
-            const createdtime = moment(existingOTP.createdAt);
-            const difftime = now.diff(createdtime, "minutes");
+  try {
+    const { email } = body;
+    const existingOTP = await OTP.findOne({ email }).sort({ _id: -1 });
+    if (existingOTP) {
+      const now = moment();
+      const createdtime = moment(existingOTP.createdAt);
+      const difftime = now.diff(createdtime, "minutes");
 
-            if (difftime < 3) {
-                return {
-                    status: httpStatus.BAD_REQUEST,
-                    message: "Please wait for 3 minutes to send otp again"
-                }
-            }
-        }
-        const otp = Math.floor(100000 + Math.random() * 900000);
-        const expires = Date.now() + 3 * 60 * 1000;
-
-        const mailbody = {
-            from: "AutoPe Payment Solution",
-            to: email,
-            subject: "Your OTP Code",
-            text: `Your OTP is ${otp} and valid for 3 minutes`
+      if (difftime < 3) {
+        return {
+          status: httpStatus.BAD_REQUEST,
+          message: "Please wait for 3 minutes to send otp again",
         };
-        const resendMail = await mailer.sendMail(mailbody);
-        console.log("Resend Mail Successfull", resendMail);
-
-        const dataForDb = {
-            email: email,
-            otp: otp,
-            expires: expires,
-        }
-        await OTP.create(dataForDb)
-        return {
-            status: httpStatus.OK,
-            message: `OTP resend successfully to ${email}`,
-        }
-    } catch (error) {
-        console.error(error?.message || error);
-        return {
-            status: httpStatus.BAD_GATEWAY,
-            message: "Failed to resend OTP"
-        }
+      }
     }
-}
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    const expires = Date.now() + 3 * 60 * 1000;
+
+    const mailbody = {
+      from: "AutoPe Payment Solution",
+      to: email,
+      subject: "Your OTP Code",
+      text: `Your OTP is ${otp} and valid for 3 minutes`,
+    };
+    const resendMail = await mailer.sendMail(mailbody);
+    console.log("Resend Mail Successfull", resendMail);
+
+    const dataForDb = {
+      email: email,
+      otp: otp,
+      expires: expires,
+    };
+    await OTP.create(dataForDb);
+    return {
+      status: httpStatus.OK,
+      message: `OTP resend successfully to ${email}`,
+    };
+  } catch (error) {
+    console.error(error?.message || error);
+    return {
+      status: httpStatus.BAD_GATEWAY,
+      message: "Failed to resend OTP",
+    };
+  }
+};
 
 const verifyOTP = async (body) => {
-    try {
-        const { email, otp } = body;
-        const record = await OTP.findOne({ email }).sort({ _id: -1 });
-        if (!record) {
-            return {
-                status: httpStatus.BAD_REQUEST,
-                message: "OTP not sent to this mail"
-            }
-        }
-        if (Date.now() > record?.expires) {
-            return {
-                status: httpStatus.BAD_REQUEST,
-                message: "OTP is Expired"
-            }
-        }
-        if (parseInt(otp) == record?.otp) {
-            record.isUsed = true;
-            await record.save();
-
-            await User.findOneAndUpdate(
-                { email },
-                {
-                    $set: {
-                        isVerified: true
-                    }
-                }
-            );
-            return {
-                status: httpStatus.OK,
-                message: "OTP verified Successfully, Now set your new Password"
-            }
-        } else {
-            return {
-                status: httpStatus.UNAUTHORIZED,
-                message: "Unauthorized"
-            }
-        }
-    } catch (error) {
-        console.error(error?.message || error);
-        return {
-            status: httpStatus.BAD_GATEWAY,
-            message: "Failed to verify OTP"
-        }
+  try {
+    const { email, otp } = body;
+    const record = await OTP.findOne({ email }).sort({ _id: -1 });
+    if (!record) {
+      return {
+        status: httpStatus.BAD_REQUEST,
+        message: "OTP not sent to this mail",
+      };
     }
+    if (Date.now() > record?.expires) {
+      return {
+        status: httpStatus.BAD_REQUEST,
+        message: "OTP is Expired",
+      };
+    }
+    if (parseInt(otp) == record?.otp) {
+      record.isUsed = true;
+      record.isVerified = true;
+      await record.save();
+
+      await User.findOneAndUpdate(
+        { email },
+        {
+          $set: {
+            isVerified: true,
+          },
+        }
+      );
+      return {
+        status: httpStatus.OK,
+        message: "OTP verified Successfully, Now set your new Password",
+      };
+    } else {
+      return {
+        status: httpStatus.UNAUTHORIZED,
+        message: "Unauthorized",
+      };
+    }
+  } catch (error) {
+    console.error(error?.message || error);
+    return {
+      status: httpStatus.BAD_GATEWAY,
+      message: "Failed to verify OTP",
+    };
+  }
 };
 
 module.exports = {
-    sentOTP,
-    verifyOTP,
-    resendOTP
-}
+  sentOTP,
+  verifyOTP,
+  resendOTP,
+};
