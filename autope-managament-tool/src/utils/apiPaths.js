fa6bae8cb9e87d583e56b8edbd5a5cf0d85201b7
Ayush Sharma <ayush.sharma@anduriltechnologies.com>
2025-09-30 12:48:34 +0530

    api envs added

--- autope-managament-tool/src/utils/apiPaths.js
+++ autope-managament-tool/src/utils/apiPaths.js
@@ -1,13 +1,79 @@
 // all the paths of api defined here
 // const env = prod;
 // BASE URL IN THE ENV
-const PROD_BASE_URL = "https://dmrc.autope.in";
+const ACTIVE_ENVIRONMENT = "staging";
+const CONFIGS = {
+  local: {
+    metro_base: "http://localhost",
+    metro_port: "3000",
+    auth_port: "3001",
+    external_port: "3002",
+    versions: {
+      auth: "v1",
+      metro: "v3",
+      external: "v2",
+    },
+  },
+  staging: {
+    metro_base: "https://staging.autope.in",
+    metro_port: "",
+    auth_port: "",
+    external_port: "",
+    versions: {
+      auth: "v1",
+      metro: "v3",
+      external: "v2",
+    },
+  },
+  staging_ip: {
+    metro_base: "http://35.200.153.72",
+    metro_port: "3000",
+    auth_port: "3001",
+    external_port: "3002",
+    versions: {
+      auth: "v1",
+      metro: "v3",
+      external: "v2",
+    },
+  },
+  production: {
+    metro_base: "https://dmrc.autope.in",
+    metro_port: "",
+    auth_port: "",
+    external_port: "",
+    versions: {
+      auth: "v1",
+      metro: "v3",
+      external: "v2",
+    },
+  },
+};
 
-// const metroPath = 3008;
-const STAGING_BASE_URL = "http://35.200.205.29:3000";
+const ACTIVE_CONFIG = CONFIGS[ACTIVE_ENVIRONMENT];
 
-const externalPath = `${STAGING_BASE_URL}/externalPath/`;
+const buildUrl = (base, port, service, version) => {
+  const portString = port ? `:${port}` : "";
+  return `${base}${portString}/${service}/${version}`;
+};
+const METRO_PATH = buildUrl(
+  ACTIVE_CONFIG.metro_base,
+  ACTIVE_CONFIG.metro_port,
+  "metro",
+  ACTIVE_CONFIG.versions.metro
+);
+const EXTERNAL_PATH = buildUrl(
+  ACTIVE_CONFIG.metro_base,
+  ACTIVE_CONFIG.external_port,
+  "external",
+  ACTIVE_CONFIG.versions.external
+);
+const AUTH_PATH = buildUrl(
+  ACTIVE_CONFIG.metro_base,
+  ACTIVE_CONFIG.auth_port,
+  "auth",
+  ACTIVE_CONFIG.versions.auth
+);
 
 export const apiPaths = {
-  updateUser: `${externalPath}/user/update-user`,
+  updateUser: `${EXTERNAL_PATH}/user/update-user`,
 };
